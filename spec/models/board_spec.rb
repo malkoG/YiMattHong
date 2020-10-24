f75a9529e6f85a226a56d8f7602832f85136460e
Jaeyeol Lee <rijgndqw012@gmail.com>
2020-10-25 01:03:05 +0900

    fix: Rewrite Model spec for ssociation (Board)

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Board, type: :model do
   describe "association" do
     it { is_expected.to have_many(:notices) }
-    it { is_expected.to belong_to(:curation) }
-    it { is_expected.not_to belong_to(:curation).optional(false) }
+    it { is_expected.not_to belong_to(:curation) }
+    it { is_expected.to belong_to(:curation).optional(true) }
   end
 end
